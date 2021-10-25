ada3ae5975862b5c5b91bb3cf51d25b4052e5d6d
hanamon <devparkhana@gmail.com>
2021-10-25 18:58:55 +0900

    [feat] server initial settings completed

---
+++ server/app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const fs = require('fs');
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+// Handling unexpected exceptions
+process.on('uncaughtException', (err) => {
+  console.log('uncaughtException : ', err);
+});
+
+// Middleware
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [process.env.CLIENT_ORIGIN],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+  })
+);
+
+// Routing
+app.get('/', (req, res) => {
+  res.send('Jisik Sponsor!');
+});
+
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error(
+    `${req.method} ${req.originalUrl} There is no router.`
+  );
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message);
+});
+
+module.exports = app;
