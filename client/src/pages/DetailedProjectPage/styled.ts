7b3a473be36592e670201ad67cc58c0abe878bce
Taewoong Na <83806948+LuckyTW@users.noreply.github.com>
2021-11-02 12:03:48 +0900

    [feat]Finalized responsive web for detailed page

--- client/src/pages/DetailedProjectPage/styled.ts
+++ client/src/pages/DetailedProjectPage/styled.ts
@@ -4,6 +4,10 @@ export const ProjectContainer = styled.div`
   background-color: beige;
   width: 100vw;
   padding: 60px 0;
+
+  @media screen and (min-width: 718px) {
+    width: 100%;
+  }
 `;
 
 export const GoTopButton = styled.a`
@@ -14,5 +18,15 @@ export const GoTopButton = styled.a`
   img {
     width: 80px;
     height: 80px;
+  }
+
+  @media screen and (max-width: 718px) {
+    bottom: 70px;
+    right: 20px;
+
+    img {
+      width: 60px;
+      height: 60px;
+    }
   }
 `;
