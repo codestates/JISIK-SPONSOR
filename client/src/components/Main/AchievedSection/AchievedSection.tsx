55018b689c27d8f13899529b96a4fdf4887cc07f
hanamon <devparkhana@gmail.com>
2021-11-05 16:06:51 +0900

    [feat] add comments

--- client/src/components/Main/AchievedSection/AchievedSection.tsx
+++ client/src/components/Main/AchievedSection/AchievedSection.tsx
@@ -7,20 +7,25 @@ import { Section, Wrap, TitleWrap } from './styled';
 import { Data, Row } from './type';
 
 const AchievedSection = () => {
+  // 변수 선언
   const [achievedProjects, setAchievedProjects] = useState<Array<Row>>([]);
 
   const getAchievedProjects = async () => {
     try {
+      // get 요청
       const response = await axios.get<Data>(
         'http://localhost:4000/projects?status=canceled&limit=3',
         { withCredentials: true }
       );
+      // 변수에 업데이트
       setAchievedProjects(response.data.projects.rows);
     } catch (err) {
+      // 에러나면 여기서 콘솔 찍힌다.
       console.error(err);
     }
   };
 
+  // 처음 랜더링 될 때 한 번 실행하는 함수 => get 요청 한 번 하겠다는 뜻
   useEffect(() => {
     getAchievedProjects();
   }, []);
@@ -32,6 +37,7 @@ const AchievedSection = () => {
           <h2>성사된 프로젝트</h2>
           <Link to="/">모두 보기</Link>
         </TitleWrap>
+        {/* 카드에 projects 를 내려준다. */}
         <Postcards projects={achievedProjects}></Postcards>
       </Wrap>
     </Section>
