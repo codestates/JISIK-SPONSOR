18f8b06bb888f794277c6bc81229124049fb6459
seongjinpark1 <blackdavil015@gmail.com>
2021-11-11 21:33:56 +0900

    [feat] add researchWord section

--- client/src/components/DetailedProject/ProjectContent/ProjectContent.tsx
+++ client/src/components/DetailedProject/ProjectContent/ProjectContent.tsx
@@ -3,7 +3,8 @@ import {
   ProjectHeader,
   ProjectWrapper,
   BudgetDetail,
-  Timeline
+  Timeline,
+  ResearchWord
 } from './styled';
 import ProjectBackground from '../../../images/project-background.png';
 import ProjectProgress from '../../../images/project-progress.png';
@@ -46,71 +47,88 @@ const ProjectContent = ({ project }: ProjectProps) => {
     });
 
   return (
-    <ProjectWrapper>
-      <ProjectHeader>
+    <>
+      <ProjectWrapper>
+        <ProjectHeader>
+          <div>
+            <img src={ProjectBackground} alt="" />
+            <h2>프로젝트 배경</h2>
+          </div>
+          <p>{project_background}</p>
+        </ProjectHeader>
+        <ProjectHeader>
+          <div>
+            <img src={ProjectProgress} alt="" />
+            <h2>프로젝트 진행상황</h2>
+          </div>
+          <p>{project_progress}</p>
+        </ProjectHeader>
+        <ProjectHeader>
+          <div>
+            <img src={ProjectGoal} alt="" />
+            <h2>프로젝트 목표</h2>
+          </div>
+          <p>{project_goals}</p>
+        </ProjectHeader>
+        <ProjectHeader>
+          <div>
+            <img src={ProjectBudget} alt="" />
+            <h2>프로젝트 예산</h2>
+          </div>
+          <p>{budget_synopsis}</p>
+          <BudgetDetail>
+            {updatedBudgetNum &&
+              updatedBudgetNum.map((budget) => {
+                return (
+                  <div key={budget.id}>
+                    <span>{budget.title}</span>
+                    <span>₩ {budget.amount}</span>
+                  </div>
+                );
+              })}
+          </BudgetDetail>
+        </ProjectHeader>
+        <ProjectHeader>
+          <div>
+            <img src={ProjectTimeline} alt="" />
+            <h2>프로젝트 타임라인</h2>
+          </div>
+          <p>{milestone_description}</p>
+          <Timeline>
+            {project_milestones &&
+              project_milestones.map((milestone) => {
+                return (
+                  <div key={milestone.id}>
+                    <span>
+                      {new Date(milestone.goal_date).toLocaleDateString(
+                        'ko-KR',
+                        {
+                          year: 'numeric',
+                          month: 'long',
+                          day: 'numeric'
+                        }
+                      )}
+                    </span>
+                    <span>{milestone.title}</span>
+                  </div>
+                );
+              })}
+          </Timeline>
+        </ProjectHeader>
+      </ProjectWrapper>
+      <ResearchWord>
         <div>
-          <img src={ProjectBackground} alt="" />
-          <h2>프로젝트 배경</h2>
+          <h2>연구자의 한 마디</h2>
+
+          <p>
+            “이번 연구는 저에게 있어 이러이러한 의미가 있는 연구로 지난 10여년
+            동안 준비해온 연구의 최종적인 것이라고 할 수 있겠습니다. 저의 연구에
+            많은 관심 부탁드리고 이것으로 인해 많은 사람들에게 도움이 되기를
+            바랍니다.”
+          </p>
         </div>
-        <p>{project_background}</p>
-      </ProjectHeader>
-      <ProjectHeader>
-        <div>
-          <img src={ProjectProgress} alt="" />
-          <h2>프로젝트 진행상황</h2>
-        </div>
-        <p>{project_progress}</p>
-      </ProjectHeader>
-      <ProjectHeader>
-        <div>
-          <img src={ProjectGoal} alt="" />
-          <h2>프로젝트 목표</h2>
-        </div>
-        <p>{project_goals}</p>
-      </ProjectHeader>
-      <ProjectHeader>
-        <div>
-          <img src={ProjectBudget} alt="" />
-          <h2>프로젝트 예산</h2>
-        </div>
-        <p>{budget_synopsis}</p>
-        <BudgetDetail>
-          {updatedBudgetNum &&
-            updatedBudgetNum.map((budget) => {
-              return (
-                <div key={budget.id}>
-                  <span>{budget.title}</span>
-                  <span>₩ {budget.amount}</span>
-                </div>
-              );
-            })}
-        </BudgetDetail>
-      </ProjectHeader>
-      <ProjectHeader>
-        <div>
-          <img src={ProjectTimeline} alt="" />
-          <h2>프로젝트 타임라인</h2>
-        </div>
-        <p>{milestone_description}</p>
-        <Timeline>
-          {project_milestones &&
-            project_milestones.map((milestone) => {
-              return (
-                <div key={milestone.id}>
-                  <span>
-                    {new Date(milestone.goal_date).toLocaleDateString('ko-KR', {
-                      year: 'numeric',
-                      month: 'long',
-                      day: 'numeric'
-                    })}
-                  </span>
-                  <span>{milestone.title}</span>
-                </div>
-              );
-            })}
-        </Timeline>
-      </ProjectHeader>
-    </ProjectWrapper>
+      </ResearchWord>
+    </>
   );
 };
 
